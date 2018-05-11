d889130043983cac55f169440e954294af4f8ea1
Stephen Lau <stephen@edument.se>
2018-05-11 23:53:42 +0200

    Put scrape into a function and beautify

--- index.js
+++ index.js
@@ -6,31 +6,36 @@ import fs from 'fs';
 
 const OUTFILE = "data.json";
 
-console.log("Preparing to scrape data...");
+//Use this function to scrape all data from the website.
+function scrapeData() {
+  console.log("Preparing to scrape data...");
 
-let skaneledenService = getSkaneledenService();
+  let skaneledenService = getSkaneledenService();
 
-skaneledenService.getAllTrackDetails()
-  .then(detailedTracks => Promise.all(
-    detailedTracks.data.items.map(dTI =>
-      skaneledenService.getTrackSegmentsDetails(dTI.slug)
-        .then(detailedTrackSegments => {
-          return Promise.all(
-            detailedTrackSegments.data.items.map(dTSI => skaneledenService.getAllTrackSegmentGeo(dTSI.slug)
-              .then(geo => ({
-                ...dTSI,
-                geo,
-              })
+  skaneledenService.getAllTrackDetails()
+    .then(detailedTracks => Promise.all(
+      detailedTracks.data.items.map(dTI =>
+        skaneledenService.getTrackSegmentsDetails(dTI.slug)
+          .then(detailedTrackSegments => {
+            return Promise.all(
+              detailedTrackSegments.data.items.map(dTSI => skaneledenService.getAllTrackSegmentGeo(dTSI.slug)
+                .then(geo => ({
+                  ...dTSI,
+                  geo,
+                })
+                )
               )
-            )
-          ).then(trackSegmentsWithGeo => ({
-            ...dTI,
-            trackSegmentsWithGeo,
-          }));
-        }))
-  ))
-  .then(tracksAndSegments => {
-    console.log(`Data extracted!  ...Writing to ${OUTFILE}`);
-    let jsonString = JSON.stringify(tracksAndSegments);
-    fs.writeFileSync(OUTFILE, jsonString);
-  });
+            ).then(trackSegmentsWithGeo => ({
+              ...dTI,
+              trackSegmentsWithGeo,
+            }));
+          }))
+    ))
+    .then(tracksAndSegments => {
+      console.log(`Data extracted!  ...Writing to ${OUTFILE}`);
+      let jsonString = JSON.stringify(tracksAndSegments, null, 2);
+      fs.writeFileSync(OUTFILE, jsonString);
+    });
+}
+
+scrapeData();

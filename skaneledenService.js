e9e0fcd9e28f62353d4035c7a4ca9c94e8d24bea
Stephen Lau <stephen@edument.se>
2018-05-11 14:49:48 +0200

    Get track data

--- skaneledenService.js
+++ skaneledenService.js
@@ -1,8 +1,29 @@
 import autoBind from 'auto-bind';
+import rp from 'request-promise';
+
+//Borrowed from skaneleden.se :) :
+const API_TOKEN = "eyJpdiI6Ikc5WVBxSlhpb0w0R3VXZzN2SVJ4eXc9PSIsInZhbHVlIjoiWWpackg5TSs2VTV6a2pmTW9yelZZckZ0dkc3YzJneWxvSVgrN1JvTHV0T3RWMUplYWNGdk5JaDRTUjJMaisrQiIsIm1hYyI6ImYwYjAwMzNkYzQ2NjljMTMzYmQwODlmNjU4YWI4ZTMxNzFiMWEyMmVlOWNmMDJlMGE1OTEwMDRiNTRkMWI0NmQifQ==";
+const ENDPOINT = "https://www.skaneleden.se/api/v2";
+const TRAIL_NAME = 'skaneleden';
+
+const TRACK_PATH = "tracks";
 
 class SkaneledenService {
   constructor() {
     autoBind(this);
+  }
+
+  getAllTrackDetails(offset, first) {
+    return rp({
+      url: `${ENDPOINT}/${TRACK_PATH}`,
+      qs: {
+        token: API_TOKEN,
+        offset,
+        limit: first,
+        areas: TRAIL_NAME,
+      },
+      json: true,
+    });
   }
 }
 
